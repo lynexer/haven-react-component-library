a01d27d2d2b1e09903d9d06d3b16b0bbf3a4992b
Kyle Graham <kylegraham05@gmail.com>
2024-09-22 13:14:25 -0700

    Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
-// rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
 
 const packageJson = require('./package.json');
+import postcss from 'rollup-plugin-postcss';
 
 export default [
     {
@@ -37,7 +36,7 @@ export default [
     {
         input: 'src/index.ts',
         output: [{ file: 'dist/index.d.ts', format: 'es' }],
-        plugins: [dts.default()]
+        plugins: [dts.default()],
         external: [/\.css$/]
     }
 ];
